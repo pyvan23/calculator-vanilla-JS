7fe907c0be689adb613964604d50c3f96ea79cee
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-01-29 12:22:13 -0300

    finish

--- calculator.js
+++ calculator.js
@@ -1,18 +1,18 @@
 //captute elements
 const numbersButtons = document.getElementsByName("data-number");
-const operationsButtons = document.getElementsByName("data-operations");
+const operationsButtons = document.getElementsByName("data-operation");
 const equalButton = document.getElementsByName("data-equal")[0];
 const deleteButton = document.getElementsByName("data-delete")[0];
 var result = document.getElementById("result");
-var currentOperation = "";
-var lastOperation = "";
+var currentOperation = " ";
+var lastOperation = " ";
 var operation = undefined;
 
-/*capture test console.log(numbersButtons);
-console.log(operationsButtons);
-console.log(equalButton[0]);
-console.log(result);
-console.log(deleteButton);*/
+//capture test console.log(numbersButtons);
+//console.log(operationsButtons);
+//console.log(equalButton);
+//console.log(result);
+//console.log(deleteButton);
 
 //events alrady define
 numbersButtons.forEach(function (button) {
@@ -34,22 +34,59 @@ equalButton.addEventListener("click", function () {
 });
 
 deleteButton.addEventListener("click", function () {
-  console.log("hi");
   clear();
   updateDisplay();
 });
 
 //define methods
+function selectOperation(op) {
+  if (currentOperation === "") {
+    return;
+  }
+  if (lastOperation !== "") {
+    calculate();
+  }
+
+  operation = op.toString();
+  lastOperation = currentOperation;
+  currentOperation = "";
+}
+function calculate() {
+  var calc;
+  const before = parseFloat(lastOperation);
+  const current = parseFloat(currentOperation);
+  if (isNaN(before) || isNaN(current)) {
+    return;
+  }
+  switch (operation) {
+    case "+":
+      calc = before + current;
+      break;
+    case "-":
+      calc = before - current;
+      break;
+    case "X":
+      calc = before * current;
+      break;
+    case "/":
+      calc = before / current;
+      break;
+  }
+  currentOperation = calc;
+  operation = undefined;
+  lastOperation = " ";
+}
 function addNumber(num) {
   currentOperation = currentOperation.toString() + num.toString();
   updateDisplay();
 }
 function clear() {
-  currentOperation = "";
-  lastOperation = "";
+  currentOperation = " ";
+  lastOperation = " ";
   operation = undefined;
 }
 function updateDisplay() {
   result.value = currentOperation;
 }
+
 clear();
